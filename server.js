72c4076c8b7b74f6bd07a357025abe9c9c0b5bfb
marcus <marcus.silva@eaportal.org>
2024-11-27 00:33:19 -0300

    conexão com o banco feita

--- server.js
+++ server.js
@@ -6,13 +6,52 @@ const porta = 3000
 
 const app = express()
 
+const {DB_HOST, DB_NAME, DB_USER, DB_PASSWORD} = process.env
+
 app.use(cors(), express.json())
 app.post("/register", (request, response) => {
     const user = request.body.user
 
-    console.log(user)
+    const searchCommand = `
+        SELECT *FROM Users
+        WHERE email = ?
+    `
+
+    db.query(searchCommand, [user.email], (error, data) => {
+        if (error){
+            console.log(error)
+            return
+        }
+
+        if (data.length !== 0){
+            response.json({message: "Já existe um usuário cadastrado com este email. Tente usar outro email.", userExists: true})
+            return
+        }
+
+        const insertCommand = `
+            INSERT INTO Users(name, email, password)
+            VALUES(?, ?, ?)
+        `
+
+        db.query(insertCommand, [user.name, user.email, user.password], (error) => {
+            if (error){
+                console.log(error)
+                return
+            }
+
+            response.json({message: "Usuário cadastrado com sucesso!"})
+        })
+    })
 })
 
 app.listen(porta, () => {
     console.log(`Servidor rodando na porta ${porta}!`);
+})
+
+const db = mysql.createPool({
+    connectionLimit: 10,
+    host: DB_HOST,
+    database:DB_NAME,
+    user:DB_USER,
+    password:DB_PASSWORD,
 })
